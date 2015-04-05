5b9054040c644b69848fcbb7fc601800c4e88e73
Houman Sarabostani <houman@lnxsrv01.seas.ucla.edu>
2015-04-05 14:57:36 -0700

    first parse

--- lab1/lab1-Skeleton/read-command.c
+++ lab1/lab1-Skeleton/read-command.c
@@ -4,26 +4,61 @@
 #include "command-internals.h"
 #include "alloc.h"
 #include "list.h"
-//#include "strutils.h"
 
 #include <error.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
-struct command_stream{
-  cmd_node_t * node;
-};
+typedef cmd_list_t command_stream;
 
 bool isword(char c){
   return strchr("!%+,-./:@^_", c) || isalpha(c) || isdigit(c);
 }
 
 bool isnewop(char c){
-  return strchr("|;&()\n",c);
+  return strchr("|;&",c);
+}
+
+void syntax_error(char col){
+  printf("syntax error near `%c'\n", col );
+  exit(1);
+}
+
+bool isblankstr(char * stream){
+  if(strlen(stream) == 0)
+    return true;
+  int i=0;
+  bool blank = false;
+  for (i = 0; i < strlen(stream); i++){
+    if(!strchr("\t ", stream[i]))
+      blank = false;
+  }
+
+  return blank;
+}
+
+STRING tostring(void * fp){
+  char c;
+  int s;
+  while((c = getc(fp)) != EOF){
+    s++;
+  }
+  STRING str = (STRING) checked_malloc(s+1);
+  s = 0;
+  rewind((FILE*)fp);
+  while((c = getc(fp)) != EOF){
+    str[s++] = c;
+  }
+  str[s] = '\0';
+  rewind((FILE*)fp);
+  return str;
 }
 
 command_t make_simple_command(char * stream){
+  if(isblankstr(stream)){
+    return NULL;
+  }
   int wchunk = 4; // initial size for words array
   char ** words = (char**)checked_malloc(wchunk*sizeof(char**));
   int bchunk = 16; // initial size for buffer(each word)
@@ -73,6 +108,8 @@ command_t make_simple_command(char * stream){
     wordcount++;
     words[w++] = wbuffer;
   }
+  if(wordcount == 0)
+    return NULL;
   command_t cmd = (command_t) malloc(sizeof(command_t));
   cmd->type = SIMPLE_COMMAND;
   cmd->status = -1;
@@ -82,7 +119,7 @@ command_t make_simple_command(char * stream){
     cmd->u.word[i] = words[i];
   }
 
-  cmd->u.word[i] = 0; //needed for execvp  
+  cmd->u.word[i] = 0; //needed for execvp
   return cmd;
 }
 
@@ -92,51 +129,86 @@ make_command_stream (int (*get_next_byte) (void *),
 {
   //get file size
   void * file_stream = get_next_byte_argument;
-  
-  bool opflag = false;  
+  cmd_list_t cmd_list = cmd_list_new();
+  str_list_t op_list = str_list_new();
+
   int chunk = 16;
   char * buffer = (char*)checked_malloc(chunk);
   int b = 0; // buffer index
   char c;
-  char pvc; //previous character
-  while((c = get_next_byte(file_stream)) != EOF){
-    if(isnewop(c)){
-      command_t cmd_a = make_simple_command(buffer);
-      cmd_list_push(cmd_list, cmd_a);
-      if(opflag){
-        opflag = false;
-        switch(pvc){
+  // convert file stream to string
+  STRING input = tostring(file_stream);
 
+  while(*input != '\0'){
+    c = *input++;
+    //if word or space or tab, put it in the buffer
+    if(isword(c) || strchr("\t ",c)){
+      if(b >= chunk){
+        chunk *= 2;
+        buffer = (char*)checked_realloc(buffer, chunk*sizeof(char**));
+      }
+      if(buffer == NULL)
+        buffer = (char*)checked_malloc(chunk);
+      buffer[b++] = c;
+    }
+    //if it's operator, push simple command and the operator
+    else if(isnewop(c)){
+      if(!isblankstr(buffer)){
+        command_t cmd_a = make_simple_command(buffer);
+        cmd_list_push(cmd_list, cmd_a);
+        b = 0;
+        buffer = NULL;
+        chunk = 16;
+      }
+      else
+        syntax_error(c);
+      // *input is now next character
+      if(c == '|' && *input == '|'){
+        printf("OR\n" );
+        str_list_push(op_list, "||");
+        input++;
+      }
+      else if(c == '&' && *input == '&'){
+        printf("AND\n");
+        str_list_push(op_list, "&&");
+        input++;
+      }
+      else if(isword(*input) || strchr("\t ", *input)){
+        switch(c){
+          case ';': str_list_push(op_list, ";");
+            break;
+          case '|' : str_list_push(op_list, "|");
+            break;
+          case '&' :str_list_push(op_list, "&");
+            break;
+          case '(' :str_list_push(op_list, "(");
+            break;
+          case ')' :str_list_push(op_list, ")");
+            break;
+          default:
+            syntax_error(c);
         }
       }
-      else{
-        pvc = c; //in case of || and &&
-        opflag = true;
-        continue;
-      }
-    }   
-    if(b >= chunk){
-      chunk *= 2;
-      buffer = (char*)checked_realloc(buffer, chunk*sizeof(char**));
     }
-    buffer[b++] = c;
   }
   //printf("buffer = %s, size=%d\n", buffer, b);
-  command_t cmd_a = make_simple_command(buffer);
-  
-
-  /*cmd_list_t cmd_list = cmd_list_new();
-  cmd_list_push(cmd_list, cmd_a);
+  if(buffer){
+    buffer[b] = c;
+    command_t cmd_b = make_simple_command(buffer);
+    //command_t cmd_b = NULL;
+    if(cmd_b)
+      cmd_list_push(cmd_list, cmd_b);
+  }
+  //printf("cmd size: %d\n", cmd_list_size(cmd_list));
   printf("%s\n", cmd_list_peek(cmd_list)->u.word[0]);
-  
-  str_list_t op_list = str_list_new();
-  str_list_push(op_list, "&&");
-  STRING str = str_list_pop(op_list);
-  printf("%d\n", str_list_size(op_list) );
+
+  //printf("%d\n", str_list_size(op_list));
+  //str_list_pop(op_list);
+  //printf("%s\n",str_list_peek(op_list));
 
 
   cmd_list_free(cmd_list);
-  str_list_free(op_list);*/
+  str_list_free(op_list);
 
   return 0;
 }
@@ -148,24 +220,3 @@ read_command_stream (command_stream_t s)
   //error (1, 0, "command reading not yet implemented");
   return 0;
 }
-
-
-/*int size = 0;
-  while(get_next_byte(file_stream) != EOF){
-    size++;
-  }
-  rewind(file_stream);
-
-  char * input = (char*) checked_malloc(size);
-
-  int i = 0;
-  char c;
-  while((c = getc(file_stream) )!= EOF){
-    input[i++] = c;
-  }
-  input[i] = '\0';
-  //2 stacks to store commands and operators
-  trim(input);*/
-
-  //char * stream = input;
-
